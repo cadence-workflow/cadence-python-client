b58f6039714bf9c0b35d5ef995578eedd6e8b62d
Shijie Sheng <liouvetren@gmail.com>
2025-08-14 12:56:59 -0700

    WIP

---
+++ cadence/async/event_loop.py
@@ -0,0 +1,40 @@
+
+
+from asyncio import AbstractEventLoop
+from asyncio import Future
+import asyncio
+
+class EventLoop(AbstractEventLoop):
+    __running = False
+
+    def __init__(self):
+        super().__init__()
+
+    def run_until_complete(self, future: Future):
+        pass
+
+    def start(self):
+        self.__running = True
+
+    def is_running(self):
+        return self.__running
+
+async def workflow():
+    asyncio.set_event_loop(EventLoop())
+
+    task1 = asyncio.create_task(delay_print("Hello, world!", 1))
+    task2 = asyncio.create_task(delay_print("Hello, world!", 1))
+    await task1
+    await task2
+
+
+async def delay_print(message, delay):
+    await asyncio.sleep(delay)
+    print(message)
+
+
+if __name__ == "__main__":
+    print("Starting workflow")
+    loop = asyncio.new_event_loop()
+    loop.start()
+    loop.run_until_complete(workflow())

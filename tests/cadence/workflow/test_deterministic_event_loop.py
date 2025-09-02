1110e1c1422d062d9f09baeb47f8cd327ab3924b
Shijie Sheng <liouvetren@gmail.com>
2025-09-02 12:57:56 -0700

    add DeterministicEventLoop  (#17)
    What changed?
    
    added DeterministEventLoop which is core of a workflow loop derived from BaseEventLoop without any I/O support.
    Why?
    
    This FIFO is needed to ensure deterministic requirement of workflows
    
    How did you test it?
    
    Unit Test

---
+++ tests/cadence/workflow/test_deterministic_event_loop.py
@@ -0,0 +1,72 @@
+import pytest
+import asyncio
+from cadence.workflow.deterministic_event_loop import DeterministicEventLoop
+
+
+async def coro_append(results: list, i: int):
+    results.append(i)
+
+async def coro_await(size: int):
+    results = []
+    for i in range(size):
+        await coro_append(results, i)
+    return results
+
+async def coro_await_future(future: asyncio.Future):
+    return await future
+
+async def coro_await_task(size: int):
+    results = []
+    for i in range(size):
+        asyncio.create_task(coro_append(results, i))
+    return results
+
+class TestDeterministicEventLoop:
+    """Test suite for DeterministicEventLoop using table-driven tests."""
+
+    def setup_method(self):
+        """Setup method called before each test."""
+        self.loop = DeterministicEventLoop()
+
+    def teardown_method(self):
+        """Teardown method called after each test."""
+        if not self.loop.is_closed():
+            self.loop.close()
+        assert self.loop.is_closed() is True
+
+    def test_call_soon(self):
+        """Test _run_once executes single callback."""
+        results = []
+        expected = []
+        for i in range(10000):
+            expected.append(i)
+            self.loop.call_soon(lambda x=i: results.append(x))
+
+        self.loop._run_once()
+
+        assert results == expected
+        assert self.loop.is_running() is False
+
+    def test_run_until_complete(self):
+        size = 10000
+        results = self.loop.run_until_complete(coro_await(size))
+        assert results == list(range(size))
+        assert self.loop.is_running() is False
+        assert self.loop.is_closed() is False
+
+    @pytest.mark.parametrize("result, exception, expected, expected_exception",
+        [(10000, None, 10000, None), (None, ValueError("test"), None, ValueError)])
+    def test_create_future(self, result, exception, expected, expected_exception):
+        future = self.loop.create_future()
+        if expected_exception is not None:
+            with pytest.raises(expected_exception):
+                future.set_exception(exception)
+                self.loop.run_until_complete(coro_await_future(future))
+        else:
+            future.set_result(result)
+            assert self.loop.run_until_complete(coro_await_future(future)) == expected
+
+    def test_create_task(self):
+        size = 10000
+        results = self.loop.run_until_complete(coro_await_task(size))
+        assert results == list(range(size))

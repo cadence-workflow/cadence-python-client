9b37a35ec3079855e9c12e6ef1746a1db53d1a3e
Tim Li <ltim@uber.com>
2026-06-16 14:43:17 -0700

    feat(metrics): emit poller and worker metrics
    Signed-off-by: Tim Li <ltim@uber.com>

--- cadence/worker/_activity.py
+++ cadence/worker/_activity.py
@@ -1,13 +1,31 @@
 import asyncio
+import time
 from typing import Optional
 
 from cadence._internal.activity import ActivityExecutor
+from cadence._internal.rpc.retry import RETRYABLE_CODES
 from cadence.api.v1.service_worker_pb2 import (
     PollForActivityTaskResponse,
     PollForActivityTaskRequest,
 )
 from cadence.api.v1.tasklist_pb2 import TaskList, TaskListKind
 from cadence.client import Client
+from cadence.error import CadenceRpcError
+from cadence.metrics import MetricsEmitter, NoOpMetricsEmitter
+from cadence.metrics.constants import (
+    ACTIVITY_POLL_COUNTER,
+    ACTIVITY_POLL_FAILED_COUNTER,
+    ACTIVITY_POLL_LATENCY,
+    ACTIVITY_POLL_NO_TASK_COUNTER,
+    ACTIVITY_POLL_SUCCEED_COUNTER,
+    ACTIVITY_POLL_TRANSIENT_FAILED_COUNTER,
+    ACTIVITY_SCHEDULED_TO_START_LATENCY,
+    POLLER_START_COUNTER,
+    TAG_DOMAIN,
+    TAG_TASK_LIST,
+    WORKER_PANIC_COUNTER,
+    WORKER_START_COUNTER,
+)
 from cadence.worker._registry import Registry
 from cadence.worker._types import WorkerOptions, _LONG_POLL_TIMEOUT
 from cadence.worker._poller import Poller
@@ -20,6 +38,11 @@ class ActivityWorker:
         self._client = client
         self._task_list = task_list
         self._identity = options["identity"]
+        self._metrics_emitter: MetricsEmitter = options.get(  # type: ignore[attr-defined]
+            "metrics_emitter", NoOpMetricsEmitter()
+        )
+        self._tags = {TAG_DOMAIN: client.domain, TAG_TASK_LIST: task_list}
+        self._num_pollers = options["activity_task_pollers"]
         max_concurrent = options["max_concurrent_activity_execution_size"]
         permits = asyncio.Semaphore(max_concurrent)
         self._executor = ActivityExecutor(
@@ -31,30 +54,61 @@ class ActivityWorker:
             options["metrics_emitter"],
         )
         self._poller = Poller[PollForActivityTaskResponse](
-            options["activity_task_pollers"], permits, self._poll, self._execute
+            self._num_pollers, permits, self._poll, self._execute
         )
         # TODO: Local dispatch, local activities, actually running activities, etc
 
     async def run(self) -> None:
-        await self._poller.run()
+        self._metrics_emitter.counter(WORKER_START_COUNTER, tags=self._tags)
+        self._metrics_emitter.counter(POLLER_START_COUNTER, self._num_pollers, tags=self._tags)
+        try:
+            await self._poller.run()
+        except Exception:
+            self._metrics_emitter.counter(WORKER_PANIC_COUNTER, tags=self._tags)
+            raise
 
     async def _poll(self) -> Optional[PollForActivityTaskResponse]:
-        task: PollForActivityTaskResponse = (
-            await self._client.worker_stub.PollForActivityTask(
-                PollForActivityTaskRequest(
-                    domain=self._client.domain,
-                    task_list=TaskList(
-                        name=self._task_list, kind=TaskListKind.TASK_LIST_KIND_NORMAL
+        self._metrics_emitter.counter(ACTIVITY_POLL_COUNTER, tags=self._tags)
+        start = time.monotonic()
+        try:
+            task: PollForActivityTaskResponse = (
+                await self._client.worker_stub.PollForActivityTask(
+                    PollForActivityTaskRequest(
+                        domain=self._client.domain,
+                        task_list=TaskList(
+                            name=self._task_list, kind=TaskListKind.TASK_LIST_KIND_NORMAL
+                        ),
+                        identity=self._identity,
                     ),
-                    identity=self._identity,
-                ),
-                timeout=_LONG_POLL_TIMEOUT,
+                    timeout=_LONG_POLL_TIMEOUT,
+                )
             )
-        )
+        except CadenceRpcError as e:
+            elapsed = time.monotonic() - start
+            self._metrics_emitter.histogram(ACTIVITY_POLL_LATENCY, elapsed, tags=self._tags)
+            if e.code in RETRYABLE_CODES:
+                self._metrics_emitter.counter(
+                    ACTIVITY_POLL_TRANSIENT_FAILED_COUNTER, tags=self._tags
+                )
+            else:
+                self._metrics_emitter.counter(ACTIVITY_POLL_FAILED_COUNTER, tags=self._tags)
+            raise
+
+        elapsed = time.monotonic() - start
+        self._metrics_emitter.histogram(ACTIVITY_POLL_LATENCY, elapsed, tags=self._tags)
 
         if task.task_token:
+            self._metrics_emitter.counter(ACTIVITY_POLL_SUCCEED_COUNTER, tags=self._tags)
+            if task.scheduled_time.seconds and task.started_time.seconds:
+                scheduled_to_start = (
+                    task.started_time.seconds + task.started_time.nanos / 1e9
+                ) - (task.scheduled_time.seconds + task.scheduled_time.nanos / 1e9)
+                self._metrics_emitter.histogram(
+                    ACTIVITY_SCHEDULED_TO_START_LATENCY, scheduled_to_start, tags=self._tags
+                )
             return task
         else:
+            self._metrics_emitter.counter(ACTIVITY_POLL_NO_TASK_COUNTER, tags=self._tags)
             return None
 
     async def _execute(self, task: PollForActivityTaskResponse) -> None:
--- cadence/worker/_decision.py
+++ cadence/worker/_decision.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from concurrent.futures import ThreadPoolExecutor
 from typing import Optional
 
@@ -8,6 +9,23 @@ from cadence.api.v1.service_worker_pb2 import (
 )
 from cadence.api.v1.tasklist_pb2 import TaskList, TaskListKind
 from cadence.client import Client
+from cadence.error import CadenceRpcError
+from cadence.metrics import MetricsEmitter, NoOpMetricsEmitter
+from cadence.metrics.constants import (
+    DECISION_POLL_COUNTER,
+    DECISION_POLL_FAILED_COUNTER,
+    DECISION_POLL_LATENCY,
+    DECISION_POLL_NO_TASK_COUNTER,
+    DECISION_POLL_SUCCEED_COUNTER,
+    DECISION_POLL_TRANSIENT_FAILED_COUNTER,
+    DECISION_SCHEDULED_TO_START_LATENCY,
+    POLLER_START_COUNTER,
+    TAG_DOMAIN,
+    TAG_TASK_LIST,
+    WORKER_PANIC_COUNTER,
+    WORKER_START_COUNTER,
+)
+from cadence._internal.rpc.retry import RETRYABLE_CODES
 from cadence.worker._decision_task_handler import DecisionTaskHandler
 from cadence.worker._poller import Poller
 from cadence.worker._registry import Registry
@@ -22,6 +40,11 @@ class DecisionWorker:
         self._task_list = task_list
         self._registry = registry
         self._identity = options["identity"]
+        self._metrics_emitter: MetricsEmitter = options.get(  # type: ignore[attr-defined]
+            "metrics_emitter", NoOpMetricsEmitter()
+        )
+        self._tags = {TAG_DOMAIN: client.domain, TAG_TASK_LIST: task_list}
+        self._num_pollers = options["decision_task_pollers"]
         permits = asyncio.Semaphore(
             options["max_concurrent_decision_task_execution_size"]
         )
@@ -32,30 +55,61 @@ class DecisionWorker:
             client, task_list, registry, executor=executor, **options
         )
         self._poller = Poller[PollForDecisionTaskResponse](
-            options["decision_task_pollers"], permits, self._poll, self._execute
+            self._num_pollers, permits, self._poll, self._execute
         )
         # TODO: Sticky poller, actually running workflows, etc.
 
     async def run(self) -> None:
-        await self._poller.run()
+        self._metrics_emitter.counter(WORKER_START_COUNTER, tags=self._tags)
+        self._metrics_emitter.counter(POLLER_START_COUNTER, self._num_pollers, tags=self._tags)
+        try:
+            await self._poller.run()
+        except Exception:
+            self._metrics_emitter.counter(WORKER_PANIC_COUNTER, tags=self._tags)
+            raise
 
     async def _poll(self) -> Optional[PollForDecisionTaskResponse]:
-        task: PollForDecisionTaskResponse = (
-            await self._client.worker_stub.PollForDecisionTask(
-                PollForDecisionTaskRequest(
-                    domain=self._client.domain,
-                    task_list=TaskList(
-                        name=self._task_list, kind=TaskListKind.TASK_LIST_KIND_NORMAL
+        self._metrics_emitter.counter(DECISION_POLL_COUNTER, tags=self._tags)
+        start = time.monotonic()
+        try:
+            task: PollForDecisionTaskResponse = (
+                await self._client.worker_stub.PollForDecisionTask(
+                    PollForDecisionTaskRequest(
+                        domain=self._client.domain,
+                        task_list=TaskList(
+                            name=self._task_list, kind=TaskListKind.TASK_LIST_KIND_NORMAL
+                        ),
+                        identity=self._identity,
                     ),
-                    identity=self._identity,
-                ),
-                timeout=_LONG_POLL_TIMEOUT,
+                    timeout=_LONG_POLL_TIMEOUT,
+                )
             )
-        )
+        except CadenceRpcError as e:
+            elapsed = time.monotonic() - start
+            self._metrics_emitter.histogram(DECISION_POLL_LATENCY, elapsed, tags=self._tags)
+            if e.code in RETRYABLE_CODES:
+                self._metrics_emitter.counter(
+                    DECISION_POLL_TRANSIENT_FAILED_COUNTER, tags=self._tags
+                )
+            else:
+                self._metrics_emitter.counter(DECISION_POLL_FAILED_COUNTER, tags=self._tags)
+            raise
+
+        elapsed = time.monotonic() - start
+        self._metrics_emitter.histogram(DECISION_POLL_LATENCY, elapsed, tags=self._tags)
 
         if task and task.task_token:
+            self._metrics_emitter.counter(DECISION_POLL_SUCCEED_COUNTER, tags=self._tags)
+            if task.scheduled_time.seconds and task.started_time.seconds:
+                scheduled_to_start = (
+                    task.started_time.seconds + task.started_time.nanos / 1e9
+                ) - (task.scheduled_time.seconds + task.scheduled_time.nanos / 1e9)
+                self._metrics_emitter.histogram(
+                    DECISION_SCHEDULED_TO_START_LATENCY, scheduled_to_start, tags=self._tags
+                )
             return task
         else:
+            self._metrics_emitter.counter(DECISION_POLL_NO_TASK_COUNTER, tags=self._tags)
             return None
 
     async def _execute(self, task: PollForDecisionTaskResponse) -> None:

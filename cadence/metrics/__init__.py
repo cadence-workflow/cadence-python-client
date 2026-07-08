01e13fe1a9b712ed09750ae47d39893d61e261d2
Tim Li <ltim@uber.com>
2026-07-08 13:29:34 -0700

    feat(metrics): configure Prometheus duration buckets
    Signed-off-by: Tim Li <ltim@uber.com>

--- cadence/metrics/__init__.py
+++ cadence/metrics/__init__.py
@@ -6,10 +6,22 @@ from .metrics import (
     NoOpMetricsEmitter,
     MetricType,
 )
+from .histogram_buckets import (
+    DEFAULT_1MS_100S,
+    HIGH_1MS_24H,
+    LOW_1MS_100S,
+    MID_1MS_24H,
+    default_buckets_for_metric,
+)
 from .prometheus import PrometheusMetrics, PrometheusConfig
 
 __all__ = [
     "duration_between_ns",
+    "DEFAULT_1MS_100S",
+    "HIGH_1MS_24H",
+    "LOW_1MS_100S",
+    "MID_1MS_24H",
+    "default_buckets_for_metric",
     "MetricsEmitter",
     "NoOpMetricsEmitter",
     "MetricType",

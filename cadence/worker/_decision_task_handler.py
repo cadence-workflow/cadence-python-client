cffef5ec339fd8bc62e760e4d95029d094642c65
Shijie Sheng <liouvetren@gmail.com>
2026-08-04 13:27:52 -0700

    fix(decision-worker): avoid missing decision task handling
    Signed-off-by: Shijie Sheng <liouvetren@gmail.com>

--- cadence/worker/_decision_task_handler.py
+++ cadence/worker/_decision_task_handler.py
@@ -363,7 +363,7 @@ class DecisionTaskHandler(BaseTaskHandler[PollForDecisionTaskResponse]):
                 task_token=task.task_token,
                 decisions=decision_result.decisions,
                 identity=self._identity,
-                return_new_decision_task=True,
+                return_new_decision_task=False,  # TODO: add optimization to handle the returned decision task if this is set to True
             )
 
             await self._client.worker_stub.RespondDecisionTaskCompleted(request)
